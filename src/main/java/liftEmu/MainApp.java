9ee60bddbf27a28ae43981f7367dd0f3f005272d
Rinefica <rinefica@gmail.com>
2019-06-12 23:43:06 +0300

    add fxml

---
+++ src/main/java/liftEmu/MainApp.java
@@ -0,0 +1,28 @@
+package liftEmu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+            MainApp.class.getResource("/MainView.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+
+        stage.setTitle("liftEmulation");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
